943a8f6fd01a71a9b626b7d6f26c2cfcb8c28519
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-08-03 17:02:35 +0300

    update

--- alx_travel_app/alx_travel_app/listings/tasks.py
+++
@@ -1,8 +0,0 @@
-# listings/tasks.py
-
-from celery import shared_task
-
-@shared_task
-def sample_task():
-    print("This is a test task from Celery")
-    return "Task complete"
--- listings/urls.py
+++
@@ -1,8 +0,0 @@
-# listings/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.api_overview, name='api-overview'),
-]
